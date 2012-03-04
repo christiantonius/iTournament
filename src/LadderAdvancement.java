5c794968808b7d2129e3ae9cb4ffecb3ca69e620
Aswin Anil <zaibutzu@gmail.com>
2012-03-05 00:31:20 +0800

    Added testdata, comments & main func for testing. Round.java also modified

--- src/LadderAdvancement.java
+++ src/LadderAdvancement.java
@@ -1,41 +1,70 @@
 import java.util.*;
 
 public class LadderAdvancement {
-
-	//read text file and get seq of teams & no of teams
 	
 	private int CurrNoOfRounds;
+	private Round[] CurrRounds; 
+	private Round[] NewRounds; 
 	
-	private Round[] CurrRounds = new Round[CurrNoOfRounds];
+	//initialize new LadderPairings system 
+	public LadderAdvancement() {
+		CurrRounds = new Round[4]; 		//The current fixtures
+		NewRounds = new Round[4/2];		//The new fixtures. Divided by 2 as the number of rounds will be halved
+	}
 	
-	private Round[] NewRounds = new Round[CurrNoOfRounds/2];
-
-	public void createBracket() {
+	//Advance teams till the final round(when current No of Rounds = 1)	
+	public void createFixtures() {
 	
-		while(CurrNoOfRounds>1) {
+		while(CurrNoOfRounds>1) { //If CurrNoOfRounds = 1, then it is already the final match
 		
-		int count1 = 0, count2 = 0;
-		while(count1 < CurrNoOfRounds) {
+			int currCount = 0, newCount = 0;
+			Round newRound = new Round();		//create a temp round to store the winners from the current
 			
-			NewRounds[count2].addteam(CurrRounds[count1].getWinner());
-			count1++;
-			NewRounds[count2].addteam(CurrRounds[count1].getWinner());
-			
-			count2++;
-			
-			//at every stage can store the data in both array in to a text file 
-		}	
-				
-		CurrRounds = NewRounds;
-		CurrNoOfRounds = CurrRounds.length;
-		NewRounds = new Round[CurrNoOfRounds/2];
-	}
+			while(currCount < CurrNoOfRounds) {
+		
+				newRound.addTeam(CurrRounds[currCount].getWinner());	//Add 1st match winner of 1st round to 1st match of 2nd round
+				currCount++;
+				newRound.addTeam(CurrRounds[currCount].getWinner());	//Add 2nd match winner of 1st round to 1st match of 2nd round
+				newRound.setWinner(newRound.getFirstTeam());			//set winner for simulation
+				NewRounds[newCount] = newRound;							
+				newCount++;												//Iterate through the array
+				currCount++;
+			}	//Now the new fixtures for one round have been created
+		
+		CurrRounds = NewRounds;						//the new fixtures we just computed becomes the current fixtures 
+		CurrNoOfRounds = CurrRounds.length;			//Update the number of rounds
+		NewRounds = new Round[CurrNoOfRounds/2];	
+		}
 	}
 	
-	Team winner = CurrRounds[0].getWinner();
-	
+	//main function to test the createFixtures function
 	public static void main( String[]args ) {
-		LadderAdvancement ladder = new LadderAdvancement();
 		
+		LadderAdvancement ladder = new LadderAdvancement();
+		Scanner sc = new Scanner(System.in);
+		
+		Round rnd;
+		Team t1, t2;
+		int NoOfTeamsFirstRound = sc.nextInt();		//read in amount of teams
+		sc.nextLine();
+		ladder.CurrNoOfRounds = NoOfTeamsFirstRound/2;
+		
+		for(int i=0; i<ladder.CurrNoOfRounds; i++) {
+			
+			rnd = new Round();
+			t1 = new Team();
+			t2 = new Team();
+			
+			t1.setTeamName(sc.nextLine());
+			t2.setTeamName(sc.nextLine());
+			
+			rnd.addTeam(t1);
+			rnd.addTeam(t2);
+			rnd.setWinner(t1);
+			ladder.CurrRounds[i] = rnd;				//Fill up the array for use in the createFixtures Function
+		}
+		
+		ladder.createFixtures();
+		System.out.println(ladder.CurrRounds[0].getWinner().checkTeamName());		//Print the winner of the final match
 	}
 }
--- src/Round.java
+++ src/Round.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Round {
 	
+	//Attributes as listed in Domain Model
 	private int startTime, duration, fieldNumber;
 	private Team winner;
 	private Vector<Team> teams = new Vector <Team>();
@@ -10,24 +11,36 @@ public class Round {
 		
 	}
 	
-	public void addteam( Team t ) {
-		if(teams.size() < 3) 
+	public void addTeam( Team t ) {
+		if(teams.size() < 2) 	//For soccer, 1 round should have at most 2 teams 
 			teams.add(t);
 	}
-
+	
+	public void setWinner( Team t ) {
+		winner = t;
+	}
+	
 	public Team getWinner() {
 		return winner;
+	}
+	
+	public Team getFirstTeam() {
+		return teams.firstElement();
+	}
+	
+	public Team getSecondTeam() {
+		return teams.elementAt(1);
 	}
 	
 	public int getStartTime() {
 		return startTime;
 	}
 	
-	public int getduration() {
+	public int getDuration() {
 		return duration;
 	}
 	
-	public int getfieldnumber() {
+	public int getFieldnumber() {
 		return fieldNumber;
 	}
 }
