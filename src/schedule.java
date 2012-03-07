2ccde33f56626ef67d1b48a77755431d38d9c79f
unknown <hahan@hahan-PC.(none)>
2012-03-07 19:22:45 +0800

    the pairing and scheduling added

---
+++ src/schedule.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.io.*;
+public class schedule{
+		Scanner sc = new Scanner(System.in);
+		String name;
+		int num;
+		public void readEvent() throws FileNotFoundException{
+			System.out.println("please type in the event name");
+			name =sc.nextLine();
+			
+			File file = new File(name+".txt");
+			Scanner scanner = new Scanner(file);
+			if(scanner.hasNext())
+				name=(scanner.nextLine());
+			if(scanner.hasNext())
+				num=Integer.parseInt(scanner.nextLine());
+			sc.close();
+		}
+		
+		public void setSchedule(){
+			int i=1;
+			while(num>1){
+				System.out.println("the round"+i+"will be on May "+(i*3+5)+"th");
+				i++;
+				num/=2;
+			}
+		}
+}
