7771f1124810413e51e230a1ffb7af72668093a1
Aswin Anil <zaibutzu@gmail.com>
2012-03-07 15:00:59 +0800

    Updated Round.java

--- src/Round.java
+++ src/Round.java
@@ -4,7 +4,7 @@ public class Round {
 	
 	//Attributes as listed in Domain Model
 	private int startTime, duration, fieldNumber;
-	private Team winner;
+	private Team winner, loser;
 	private Vector<Team> teams = new Vector <Team>();
 	
 	public Round() {
@@ -16,12 +16,29 @@ public class Round {
 			teams.add(t);
 	}
 	
+	public boolean contains(Team t) {
+		return teams.contains(t);
+	}
+	
 	public void setWinner( Team t ) {
 		winner = t;
+		int winnerIndex = teams.indexOf(t);
+		if(winnerIndex ==1)
+			loser = teams.elementAt(0);
+		else
+			loser = teams.elementAt(1);
+	}
+	
+	public int getSize() {
+		return teams.size();
 	}
 	
 	public Team getWinner() {
 		return winner;
+	}
+	
+	public Team getLoser() {
+		return loser;
 	}
 	
 	public Team getFirstTeam() {
